071291221ae82784a3323299b5bc7b6b27d00d7a
acen20 <ahsen1330@gmail.com>
2021-07-18 23:34:33 +0500

    Heroku

--- DiseasePrediction/settings.py
+++ DiseasePrediction/settings.py
@@ -30,7 +30,7 @@ DEBUG = True
 ALLOWED_HOSTS = []
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 
 
